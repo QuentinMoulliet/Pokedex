5dd37a3fb641d84365caa442bcfbdb2bba9cb7ff
Quentin Moulliet <quentin.moulliet.dev@gmail.com>
2024-07-17 16:55:47 +0200

    add evolution in bubble

--- js/main.js
+++ js/main.js
@@ -32,6 +32,7 @@ const main = {
             const image = document.createElement('img');
             image.id = 'hover-image';
             image.src = pokemon.sprites.regular;
+
             const imageContainer = document.createElement('div');
             imageContainer.className = 'card-image';
             imageContainer.appendChild(image);
@@ -57,18 +58,17 @@ const main = {
             generation.textContent = pokemon.generation;
             cardElement.appendChild(generation);
 
-            main.handleCreateInfoBubble(cardElement, pokemon);
+            main.handleCreateInfoBubble(cardElement, pokemon, pokemons);
         });
     },
 
-    handleCreateInfoBubble: function (cardElement, pokemon) {
+    handleCreateInfoBubble: function (cardElement, pokemon, pokemons) {
         const infoBubble = document.createElement('div');
         infoBubble.classList.add('bubble');
         cardElement.appendChild(infoBubble);
 
         cardElement.addEventListener('click', function (event) {
             event.stopPropagation();
-            console.log(pokemon);
 
             if (main.currentInfoBubble) {
                 main.currentInfoBubble.style.display = 'none';
@@ -76,7 +76,7 @@ const main = {
             infoBubble.style.display = (infoBubble.style.display === 'none' || infoBubble.style.display === '') ? 'block' : 'none';
             main.currentInfoBubble = infoBubble;
 
-            main.handleAddInfoBubble(infoBubble, pokemon);
+            main.handleAddInfoBubble(infoBubble, pokemon, pokemons);
 
             const closeButton = document.createElement('span');
             closeButton.textContent = '✖';
@@ -90,8 +90,8 @@ const main = {
         });
     },
 
-    handleAddInfoBubble: function (infoBubble, pokemon) {
-        
+    handleAddInfoBubble: function (infoBubble, pokemon, pokemons) {
+
 
         infoBubble.innerHTML = '';
 
@@ -195,14 +195,28 @@ const main = {
         }
         statRight.appendChild(talents);
 
+        const statBarContainer = document.createElement('div');
+        statBarContainer.className = 'stat-bar-container';
+        infoRight.appendChild(statBarContainer);
+
+        const statBarContainerLeft = document.createElement('div');
+        statBarContainerLeft.className = 'stat-bar-container-left';
+        statBarContainer.appendChild(statBarContainerLeft);
+
+        const statTitle = document.createElement('h4');
+        statTitle.textContent = 'Statistiques';
+        statTitle.id = 'stat-title';
+        statBarContainerLeft.appendChild(statTitle);
+
         const statBar = document.createElement('div');
         statBar.id = 'stat-bar';
+        statBarContainerLeft.appendChild(statBar);
 
         const hp = document.createElement('p');
         hp.textContent = 'PV : ' + pokemon.stats.hp;
         statBar.appendChild(hp);
         const hpBarContainer = document.createElement('div');
-        hpBarContainer.classList.add('stat-bar-container');
+        hpBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(hpBarContainer);
         const hpBar = document.createElement('div');
         hpBar.classList.add('stat-bar');
@@ -213,7 +227,7 @@ const main = {
         attack.textContent = 'Attaque : ' + pokemon.stats.atk;
         statBar.appendChild(attack);
         const attackBarContainer = document.createElement('div');
-        attackBarContainer.classList.add('stat-bar-container');
+        attackBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(attackBarContainer);
         const attackBar = document.createElement('div');
         attackBar.classList.add('stat-bar');
@@ -224,7 +238,7 @@ const main = {
         defense.textContent = 'Défense : ' + pokemon.stats.def;
         statBar.appendChild(defense);
         const defenseBarContainer = document.createElement('div');
-        defenseBarContainer.classList.add('stat-bar-container');
+        defenseBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(defenseBarContainer);
         const defenseBar = document.createElement('div');
         defenseBar.classList.add('stat-bar');
@@ -235,7 +249,7 @@ const main = {
         specialAttack.textContent = 'Atq Spé : ' + pokemon.stats.spe_atk;
         statBar.appendChild(specialAttack);
         const specialAttackBarContainer = document.createElement('div');
-        specialAttackBarContainer.classList.add('stat-bar-container');
+        specialAttackBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(specialAttackBarContainer);
         const specialAttackBar = document.createElement('div');
         specialAttackBar.classList.add('stat-bar');
@@ -246,7 +260,7 @@ const main = {
         specialDefense.textContent = 'Déf Spé : ' + pokemon.stats.spe_def;
         statBar.appendChild(specialDefense);
         const specialDefenseBarContainer = document.createElement('div');
-        specialDefenseBarContainer.classList.add('stat-bar-container');
+        specialDefenseBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(specialDefenseBarContainer);
         const specialDefenseBar = document.createElement('div');
         specialDefenseBar.classList.add('stat-bar');
@@ -257,14 +271,72 @@ const main = {
         speed.textContent = 'Vitesse : ' + pokemon.stats.vit;
         statBar.appendChild(speed);
         const speedBarContainer = document.createElement('div');
-        speedBarContainer.classList.add('stat-bar-container');
+        speedBarContainer.classList.add('stat-bar-details');
         statBar.appendChild(speedBarContainer);
         const speedBar = document.createElement('div');
         speedBar.classList.add('stat-bar');
         speedBar.style.width = (pokemon.stats.vit * 100 / 255) + '%';
-
         speedBarContainer.appendChild(speedBar);
-        infoRight.appendChild(statBar);
+
+        const statBarContainerRight = document.createElement('div');
+        statBarContainerRight.className = 'stat-bar-container-right';
+        statBarContainer.appendChild(statBarContainerRight);
+
+        const evolutionTitle = document.createElement('h4');
+        evolutionTitle.textContent = 'Évolution';
+        evolutionTitle.id = 'evolution-title';
+        statBarContainerRight.appendChild(evolutionTitle);
+
+        // Création d'un conteneur pour les conditions d'évolution
+        const evolutionConditionsContainer = document.createElement('div');
+        evolutionConditionsContainer.id = 'evolution-conditions-container';
+
+        // Vérification et gestion des évolutions précédentes
+        if (pokemon.evolution.pre && pokemon.evolution.pre.length > 0) {
+            pokemon.evolution.pre.forEach((preEvolution) => {
+                
+                const preEvolutionName = document.createElement('p');
+                preEvolutionName.id = 'pre-evolution-name';
+                preEvolutionName.textContent = preEvolution.name;
+                evolutionConditionsContainer.appendChild(preEvolutionName);
+
+                const preEvolutionImage = document.createElement('img');
+                preEvolutionImage.id = 'pre-evolution-image';
+                preEvolutionImage.src = pokemons[preEvolution.pokedex_id-1].sprites.regular;
+                evolutionConditionsContainer.appendChild(preEvolutionImage);
+
+                const preEvolutionCondition = document.createElement('p');
+                preEvolutionCondition.id = 'pre-evolution-condition';
+                preEvolutionCondition.textContent = preEvolution.condition;
+                evolutionConditionsContainer.appendChild(preEvolutionCondition);
+
+            });
+        }
+
+
+        // Vérification et gestion des évolutions suivantes
+        if (pokemon.evolution.next && pokemon.evolution.next.length > 0) {
+            pokemon.evolution.next.forEach((nextEvolution) => {
+
+                const nextEvolutionName = document.createElement('p');
+                nextEvolutionName.id = 'next-evolution-name';
+                nextEvolutionName.textContent = nextEvolution.name;
+                evolutionConditionsContainer.appendChild(nextEvolutionName);
+
+                const nextEvolutionImage = document.createElement('img');
+                nextEvolutionImage.id = 'next-evolution-image';
+                nextEvolutionImage.src = pokemons[nextEvolution.pokedex_id-1].sprites.regular; 
+                evolutionConditionsContainer.appendChild(nextEvolutionImage);
+
+                const nextEvolutionCondition = document.createElement('p');
+                nextEvolutionCondition.id = 'next-evolution-condition';
+                nextEvolutionCondition.textContent = nextEvolution.condition;
+                evolutionConditionsContainer.appendChild(nextEvolutionCondition);
+            });
+        }
+        // Ajout du conteneur des conditions d'évolution au conteneur principal
+        statBarContainerRight.appendChild(evolutionConditionsContainer);
+
     },
 
     handleCreateAboutBubble: function () {
